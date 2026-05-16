44e3add523635901d50ef27548759853943e56ca
Tomascinturion <166553758+Tomascinturion@users.noreply.github.com>
2026-05-16 17:04:43 -0300

    Entrega2

---
+++ entrega2.py
@@ -0,0 +1,27 @@
+import itertools
+
+from simpleai.search import (CspProblem, backtrack, min_conflicts,
+                             MOST_CONSTRAINED_VARIABLE,
+                             LEAST_CONSTRAINING_VALUE,
+                             HIGHEST_DEGREE_VARIABLE)
+def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
+    ...
+    variables = (
+        #casillas
+    )
+    for i in range(camp_size[0]):
+        for j in range(camp_size[1]):
+            if (i, j) not in craters:
+                variables += (i,j)
+    dominios = {
+        #modulos por celda
+    }
+    for var in variables:
+        dominios[var] = ["hab", "generator", "lab", "deposit", "airlock", "empty"]
+
+    restricciones = []
+    #seguir restricciones
+    def seguridad_energetica(vars, values):
+        if generators in values and habs in values:
+            pass
+        ...
